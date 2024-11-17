0d193ee02244224d112f46939778715710fd6ddc
Adriano Mochama <adriano@ibuqa.io>
2024-11-17 19:54:54 +0300

    chore: add husky for pre-commit checks

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
+	console.log('REQUEST', req.method, req.url);
 	const movie = { id: 1, name: 'Star Wars' };
 	return NextResponse.json(movie, { status: 200 });
 }
--- src/server/data/migrations/0000_omniscient_siren.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS "nursery_users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"age" integer
-);
